3591035b4a4d2d0111c4dff1466601410979a9da
pedro-git-projects <pedro.generic.email@gmail.com>
2022-06-16 18:06:34 -0300

    iteration exercises

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,7 @@
 package iteration
 
-const nTimes = 5
-
-func Repeat(toRepeat string) string {
+// Repeat takes a string s and an int n and repeats the string n times
+func Repeat(toRepeat string, nTimes int) string {
 	var repeated string
 	for i := 0; i < nTimes; i++ {
 		repeated += toRepeat
